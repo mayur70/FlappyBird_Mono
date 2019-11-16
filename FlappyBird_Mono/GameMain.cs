116d05aeb094bfd08205c6f4110db1ba4194e7fd
Mayur Patil <mayurpatil001@hotmail.com>
2019-11-16 21:16:38 +0530

    The Parallax Update

--- FlappyBird_Mono/GameMain.cs
+++ FlappyBird_Mono/GameMain.cs
@@ -6,9 +6,6 @@ using ScalingClever;
 
 namespace FlappyBird_Mono
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class GameMain : Game
     {
         private const int WINDOW_WIDTH = 1280;
@@ -22,7 +19,15 @@ namespace FlappyBird_Mono
 
 
         private Texture2D background;
+        private float backgroundScroll = 0f;
+        
         private Texture2D ground;
+        private float groundScroll = 0f;
+
+        private const int BACKGROUND_SCROLL_SPEED = 30;
+        private const int GROUND_SCROLL_SPEED = 60;
+
+        private const int BACKGROUND_LOOPING_POINT = 413;
 
         private IInputHandler input;
 
@@ -82,6 +87,10 @@ namespace FlappyBird_Mono
             if (input.GamePadState.Buttons.Back == ButtonState.Pressed || input.KeyboardState.IsKeyDown(Keys.Escape))
                 Exit();
 
+            float delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * delta) % BACKGROUND_LOOPING_POINT;
+            groundScroll = (groundScroll + GROUND_SCROLL_SPEED * delta) % VIRTUAL_WIDTH;
         
             base.Update(gameTime);
         }
@@ -92,8 +101,8 @@ namespace FlappyBird_Mono
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
 
-            spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
-            spriteBatch.Draw(ground, new Vector2(0, VIRTUAL_HEIGHT - 16), Color.White);
+            spriteBatch.Draw(background, new Vector2(-backgroundScroll, 0), Color.White);
+            spriteBatch.Draw(ground, new Vector2(-groundScroll, VIRTUAL_HEIGHT - 16), Color.White);
 
             spriteBatch.End();
             base.Draw(gameTime);
